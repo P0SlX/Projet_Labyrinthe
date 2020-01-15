190d58a8719690e9a538b067ce404c1e426116c3
P0SlX <25037788+P0SlX@users.noreply.github.com>
2020-01-15 17:49:56 +0100

    listeJoueurs fini

--- labyrinthe/versionClassique/joueur.py
+++ labyrinthe/versionClassique/joueur.py
@@ -15,7 +15,7 @@ def Joueur(nom):
     paramètre: nom une chaine de caractères
     retourne le joueur ainsi créé
     """
-    return {"Nom":nom, "Liste de trésor": []}
+    return {"Nom":nom, "Liste de trésor": [], "numJoueur": 0}
 
 def ajouterTresor(joueur,tresor):
     """
